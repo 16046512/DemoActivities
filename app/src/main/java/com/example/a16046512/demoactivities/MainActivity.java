6f709a389d5984a70b11d61435215bfae3adf620
16046512 <16046512@myrp.edu.sg>
2018-04-16 14:56:42 +0800

    Finish

--- app/src/main/java/com/example/a16046512/demoactivities/MainActivity.java
+++ app/src/main/java/com/example/a16046512/demoactivities/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.a16046512.demoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnDone = (Button)findViewById(R.id.btnDone);
+        EditText name = (EditText)findViewById(R.id.etName);
+        EditText age = (EditText)findViewById(R.id.etage);
+
+        if(name.getText().toString() != "" && age.getText().toString() != "") {
+            btnDone.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    EditText name = (EditText) findViewById(R.id.etName);
+                    EditText age = (EditText) findViewById(R.id.etage);
+                    String[] info = {name.getText().toString(), age.getText().toString()};
+
+                    Intent i = new Intent(MainActivity.this, DemoActivities2.class);
+
+                    i.putExtra("info", info);
+                    startActivity(i);
+
+                }
+            });
+        }else{
+            Toast.makeText(MainActivity.this, "Please fill in name and age!",
+                    Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
